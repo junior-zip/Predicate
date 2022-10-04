899fafa2c42205c7c2242fd8a120c4441f5aa351
Junior Teixeira <junior26bmx@gmail.com>
2022-10-04 13:16:24 -0300

    Usando interface funcional

---
+++ src/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.00));
+        list.add(new Product("HD Case", 80.90));
+
+
+        Double min = 100.0;
+
+        list.removeIf(p -> p.getPrice() >= min);
+
+        for (Product p: list){
+            System.out.println(p);
+        }
+
+    }
+
+
+}
