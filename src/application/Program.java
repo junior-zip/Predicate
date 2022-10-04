d537c5d6eae86fec5b726f3c11de2912fc75b049
Junior Teixeira <junior26bmx@gmail.com>
2022-10-04 16:38:33 -0300

    Expressao lambda declarada in line

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,7 @@ import entities.Product;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Program {
     public static void main(String[] args) {
@@ -15,7 +16,11 @@ public class Program {
         list.add(new Product("Tablet", 350.00));
         list.add(new Product("HD Case", 80.90));
 
-        list.forEach(Product::nonStaticPriceUpdate);
+        double factor = 1.1;
+
+        Consumer<Product> cons = p -> p.setPrice(p.getPrice() * factor);
+
+        list.forEach(cons);
 
         list.forEach(System.out::println);
 
